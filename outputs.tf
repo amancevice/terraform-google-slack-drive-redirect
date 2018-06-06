2fef17ea9176f5be768f2c7784fdd4f159643faf
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-06 14:22:48 -0400

    correct package.json

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "version" {
+  description = "Slack Drive redirect module version"
+  value       = "${local.version}"
+}
+
 output "redirect_url" {
   description = "Endpoint of redirect for slash command links to configure in Slack Drive."
   value       = "${google_cloudfunctions_function.function.https_trigger_url}"
